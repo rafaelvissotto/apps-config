3fa64ff922e3127fe46bb737a1fbc6655cc9cab2
rafaelvissotto <rafaelvissotto@gmail.com>
2023-11-18 14:34:23 -0300

    update config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.color_scheme = 'Bright Lights'
+config.font = wezterm.font('FiraCode Nerd Font Mono')
+config.font_size = 12
+config.hide_tab_bar_if_only_one_tab = true
+
+config.scrollback_lines = 5000
+config.enable_scroll_bar = true
+
+config.keys = {
+	{
+		key = '(',
+		mods = 'CTRL|SHIFT',
+		action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+	},
+	{
+		key = ')',
+		mods = 'CTRL|SHIFT',
+		action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+	},
+}
+
+return config

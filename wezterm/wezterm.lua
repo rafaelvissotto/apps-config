219a69e4ae4a5f33c06ec079b818af217b0ec323
Rafael Vissotto <rafaelvissotto@gmail.com>
2025-03-23 21:38:32 -0300

    adding wezterm and zsh configs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,26 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+local act = wezterm.action
 
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Bright Lights'
 config.font = wezterm.font('FiraCode Nerd Font Mono')
 config.font_size = 12
-config.default_cursor_style = 'BlinkingBar'
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.scrollback_lines = 5000
+config.enable_scroll_bar = true
+
+config.keys = {
+	{
+		key = '(',
+		mods = 'CTRL|SHIFT',
+		action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+	},
+	{
+		key = ')',
+		mods = 'CTRL|SHIFT',
+		action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+	},
+}
 
 return config

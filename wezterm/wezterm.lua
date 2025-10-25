6154ea1593752ff37ce2ec6a060e4cbfdaddf0db
Rafael Vissotto <rafaelvissotto@gmail.com>
2025-10-25 09:34:53 -0300

    updating wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,9 +6,21 @@ local scheme = wezterm.color.get_builtin_schemes()["Bright Lights"]
 scheme.brights[1] = "Gray" -- great theme, but I cannot read commented lines...
 config.colors = scheme
 
+config.window_background_opacity = 0.9
+config.text_background_opacity = 0.7
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+config.use_fancy_tab_bar = true
+
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 11
-config.hide_tab_bar_if_only_one_tab = true
 
 config.scrollback_lines = 10000
 config.enable_scroll_bar = true
@@ -48,6 +60,11 @@ config.keys = {
 		key = "F",
 		mods = "CTRL|SHIFT",
 		action = act.Search({ Regex = "" }),
+	},
+	{
+		key = "w",
+		mods = "CTRL",
+		action = act.CloseCurrentPane({ confirm = true }),
 	},
 }
 

e3586e3fbcf88b3a1e05b4748f87d43e2b4b241f
Rafael Vissotto <rafaelvissotto@gmail.com>
2025-12-01 08:52:25 -0300

    Adding yazi config to list folders keeping terminal context with "lf"

--- zsh/zsh_functions.sh
+++ zsh/zsh_functions.sh
@@ -74,3 +74,11 @@ function update_prompt() {
 
     PROMPT+="$PROMPT_BASE"
 }
+
+function lf() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
